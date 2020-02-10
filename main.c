301109efcee6611f7a3f6d1dfa6fda9d7da62ba3
RuslanRussel <ktrrasel86@gmail.com>
2020-02-11 00:04:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,3 @@
-/******************************************************************************
- * File Name:   main.c
- *
- * Description: This is the source code for the Empty PSoC6 Application
- *              for ModusToolbox.
- *
- * Related Document: See Readme.md
- *
- *
- *******************************************************************************
- * (c) 2019, Cypress Semiconductor Corporation. All rights reserved.
- *******************************************************************************
- * This software, including source code, documentation and related materials
- * ("Software"), is owned by Cypress Semiconductor Corporation or one of its
- * subsidiaries ("Cypress") and is protected by and subject to worldwide patent
- * protection (United States and foreign), United States copyright laws and
- * international treaty provisions. Therefore, you may use this Software only
- * as provided in the license agreement accompanying the software package from
- * which you obtained this Software ("EULA").
- *
- * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
- * non-transferable license to copy, modify, and compile the Software source
- * code solely for use in connection with Cypress's integrated circuit products.
- * Any reproduction, modification, translation, compilation, or representation
- * of this Software except as specified above is prohibited without the express
- * written permission of Cypress.
- *
- * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
- * reserves the right to make changes to the Software without notice. Cypress
- * does not assume any liability arising out of the application or use of the
- * Software or any product or circuit described in the Software. Cypress does
- * not authorize its products for use in any products where a malfunction or
- * failure of the Cypress product may reasonably be expected to result in
- * significant property damage, injury or death ("High Risk Product"). By
- * including Cypress's product in a High Risk Product, the manufacturer of such
- * system or application assumes all risk of such use and in doing so agrees to
- * indemnify Cypress against all liability.
- *******************************************************************************/
-
 #include "cy_pdl.h"
 #include "cyhal.h"
 #include "cybsp.h"
@@ -48,10 +7,10 @@
 #define CELSIUS (false)
 #define FAHRENHEIT (true)
 #define TX_PERIOD_30_S (29975100u)  //30 seconds
-#define TX_PERIOD_10_S (9991700u)   //10 seconds
-#define TX_PERIOD_5_S (499585u)     //5 seconds
-#define TX_PERIOD_1_S (999170u)     //1 second
-#define LED_INDICATION_TIME (499u)  // milisecond second of LED on
+#define TX_PERIOD_10_S (9991700u)    //10 seconds
+#define TX_PERIOD_5_S (499585u)        //5 seconds
+#define TX_PERIOD_1_S (999170u)        //1 second
+#define LED_INDICATION_TIME (499u)   // milisecond second of LED on
 
 const float multiply_temperature_koefficient = 0.0677568;
 const float temperature_offset = 43.65;
@@ -76,13 +35,13 @@ void handle_error(void) {
 }
 
 /*---------------------------------------*/
-static float32_t get_temp_data(bool celsius_or_fahrenheit);	                      //CELSIUS==false, FAHRENHEIT==true
-static void timer_init(uint32_t period);		                                  //timer set configuration data
+static float32_t get_temp_data(bool celsius_or_fahrenheit);	                                   //CELSIUS==false, FAHRENHEIT==true
+static void timer_init(uint32_t period);		                                                           //timer set configuration data
 //parameter [period] the period between transactions
 static void gpio_interrupt_handler(void *handler_arg, cyhal_gpio_event_t event);  //Button interrupt handler
-static void isr_timer(void *callback_arg, cyhal_timer_event_t event);             //Timer interrupt handler
-static void button_init(void);				                                      //Button config and start listener
-static void analog_to_digital_hw_init(void);		                              //analog to digital hw setup
+static void isr_timer(void *callback_arg, cyhal_timer_event_t event);                       //Timer interrupt handler
+static void button_init(void);				                                                                   //Button config and start listener
+static void analog_to_digital_hw_init(void);		                                                   //analog to digital hw setup
 /*---------------------------------------*/
 
 int main(void) {
@@ -155,12 +114,12 @@ static float32_t get_temp_data(bool celsius_or_fahrenheit) {
 static void timer_init(uint32_t period) {
 	cy_rslt_t result;
 	const cyhal_timer_cfg_t led_blink_timer_cfg = { .compare_value =
-	LED_INDICATION_TIME,             /* Timer compare value, not used */
-	.period = period,                /* Defines the timer period */
-	.direction = CYHAL_TIMER_DIR_UP, /* Timer counts up */
-	.is_compare = false, 			 /* Don't use compare mode */
-	.is_continuous = true, 			 /* Run timer indefinitely */
-	.value = 0 						 /* Initial value of counter */
+	LED_INDICATION_TIME,                       // Timer compare value, not used 
+	.period = period,                                      //  Defines the timer period 
+	.direction = CYHAL_TIMER_DIR_UP,  // Timer counts up 
+	.is_compare = false, 			          //  Don't use compare mode 
+	.is_continuous = true, 			          //  Run timer indefinitely 
+	.value = 0 						  // Initial value of counter 
 	};
 
 	result = cyhal_timer_init(&led_blink_timer, (cyhal_gpio_t) NC, NULL);
@@ -176,9 +135,9 @@ static void timer_init(uint32_t period) {
 
 static void button_init(void) {
 	cyhal_gpio_init((cyhal_gpio_t) BTN_HAL_PORT_PIN, BTN_HAL_DIR,
-	BTN_HAL_DRIVEMODE, CYBSP_BTN_OFF);								//Init BTN
+	BTN_HAL_DRIVEMODE, CYBSP_BTN_OFF);					
 	cyhal_gpio_register_callback((cyhal_gpio_t) BTN_HAL_PORT_PIN,
-			gpio_interrupt_handler, NULL);							//BTN Interrupt
+			gpio_interrupt_handler, NULL);							
 	cyhal_gpio_enable_event((cyhal_gpio_t) BTN_HAL_PORT_PIN,
 			CYHAL_GPIO_IRQ_FALL, INTERRUPT_PRIORITY, true);
 }
